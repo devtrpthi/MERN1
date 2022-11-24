a8a5be81d19cdf31a8c832c3efe5474003eed438
devesh <deveshkrishnatripathi@gmail.com>
2022-11-24 18:46:57 +0530

    post:routes

--- server/index.js
+++ server/index.js
@@ -1,16 +1,19 @@
- import express from 'express';
- import bodyParser from 'body-parser';
- import cors from 'cors';
- import mongoose, { Mongoose } from 'mongoose';
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import   mongoose  from 'mongoose';
+import postRoutes from './routes/post.js';
 
 const app = express();
 
+
+app.use('/post', postRoutes);
  app.use(bodyParser.json({limit: '30mb', extended: true}));
  app.use(bodyParser.urlencoded({limit: '30mb', extended: true}));
  app.use(cors());
 
 const CONNECTION_URL =  'mongodb+srv://devtrpthi:Devesh1234@cluster1.itaib5r.mongodb.net/?retryWrites=true&w=majority';
- const PORT = process.env.PORT || 5000;
+ const PORT = process.env.PORT || 3000;
 
  mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
   .then(() => app.listen(PORT, () => console.log(`Server is running on ${PORT}`)))
