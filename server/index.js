9e5833351849a4f4f179ae967225b3c121c6493b
devesh <deveshkrishnatripathi@gmail.com>
2022-11-24 18:26:12 +0530

    connect:db

--- server/index.js
+++ server/index.js
@@ -1,7 +1,22 @@
  import express from 'express';
  import bodyParser from 'body-parser';
  import cors from 'cors';
- import mongoose from 'mongoose';
+ import mongoose, { Mongoose } from 'mongoose';
+
+const app = express();
+
+ app.use(bodyParser.json({limit: '30mb', extended: true}));
+ app.use(bodyParser.urlencoded({limit: '30mb', extended: true}));
+ app.use(cors());
+
+const CONNECTION_URL =  'mongodb+srv://devtrpthi:Devesh1234@cluster1.itaib5r.mongodb.net/?retryWrites=true&w=majority';
+ const PORT = process.env.PORT || 5000;
+
+ mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+  .then(() => app.listen(PORT, () => console.log(`Server is running on ${PORT}`)))
+  .catch((error) => console.log(error.message)); 
+
+
 
 
 
